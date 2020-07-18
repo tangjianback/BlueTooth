b6cde28ead0265c33c372bb6f15a202f277123e4
tangjianback <tangjians@icloud.com>
2020-07-18 17:43:07 +0800

    tangjian code

---
+++ app/src/main/java/com/tj/bluetooth/Test.java
@@ -0,0 +1,11 @@
+package com.tj.bluetooth;
+
+import android.util.Log;
+
+public class Test {
+    String TAG = "Test";
+    Test()
+    {
+        Log.d(TAG, "Test: "+"this is a test file");
+    }
+}
